62db8e638e232138393b9c8999996bd4c51defe5
O-Theu <omateus.conceicao@gmail.com>
2023-02-07 23:33:35 -0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import dados from './mock.json';
+
 
 function App() {
+  const modulos = Object.keys(dados);
+
+  const [searchText, setSearchText] = useState('');
+  const [open, setOpen] = useState(false);
+
+  function onChangeInputText(e) {
+    setSearchText(e.target.value);
+    setOpen(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <input className="search" type="text" onClick={onChangeInputText} onChange={onChangeInputText}/>
+      {open ? (<div className='modal'>
+        {
+          modulos.map(modulo => {
+            return(
+              <ul key={modulo}>
+                <h2 className='titulo'>{modulo}</h2>
+                {
+                  dados[modulo].filter((item) => {
+                    return item.nome.includes(searchText.toLowerCase())
+                  }).map((item, index) => (
+                    <li className="link" key={index}>{item.nome}</li>
+                  ))
+                }
+              </ul>
+            )
+          })
+        }
+      </div>)
+      :
+      (<></>)
+      }
     </div>
   );
 }
